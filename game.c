05fb98614421f38a7807a97d5d30c23cf9971e35
cocosilex <141557663+cocosilex@users.noreply.github.com>
2025-11-18 19:09:16 +0100

    improve some features + some additions
    - refactor death code
    - add start screen (minimal at this time)
    - add minimal amount of columns for playing, playing with less columns will result in a degraded playing experience
    
    planning to implement like 3 difficulties.

--- game.c
+++ game.c
@@ -1,5 +1,6 @@
 #include <ncurses.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <locale.h>
@@ -10,16 +11,19 @@ bool continue_to_play = true;
 
 
 void init(void);
+void start_menu(void);
 bool game(void);
 void check_cheat(void);
 void initialize_borders(unsigned *borders);
 void update_borders(unsigned *borders, unsigned score);
 void moov_cursor(int key, unsigned *x_pos, unsigned *borders);
+void death(unsigned x_pos, unsigned long score);
 
 int main() {
     setlocale(LC_ALL, "");
     init();
 
+    start_menu();
     while (continue_to_play) {
         bool will_to_continue = game();
         if(!will_to_continue) {
@@ -49,9 +53,37 @@ void init(void) {
     init_pair(2, 1, -1);
     init_pair(3, 6, -1);
     init_pair(4, 52, -1);
+    init_pair(5, 43, -1);
 
     width = COLS/5;
     srand(time(NULL));
+}
+
+void start_menu(void) {
+    timeout(-1);
+
+    attron(A_BOLD | A_UNDERLINE | COLOR_PAIR(5));
+    mvprintw(LINES/10, COLS/2 - 4, "The Infernal Canyon");
+    attroff(A_BOLD | A_UNDERLINE | COLOR_PAIR(5));
+
+    attron(A_ITALIC| COLOR_PAIR(5));
+    mvprintw(LINES/10 + 1, COLS/2 - 1, "by @cocosilex");
+    attroff(A_ITALIC | COLOR_PAIR(5));
+
+    attron(A_ITALIC| COLOR_PAIR(5));
+    mvprintw(LINES/2 - LINES/10 - 1, COLS/2 - 8, "Informations about the game :");
+    mvprintw(LINES/2 - LINES/10, COLS/2 - 30, "Keybinds system : press 'd' to move to the right and 'q' to move to the left.");
+    mvprintw(LINES/2 - LINES/10 + 1, COLS/2 - 35, "The speed of the game is progressive, starting at 50 mps or 'move per second' of the grid.");
+    mvprintw(LINES/2 - LINES/10 + 2, COLS/2 - 13, "The score is incremented by one at each move of the grid.");
+    mvprintw(LINES/2 - LINES/10 + 4, COLS/2 - 7, "Press 'r' to start playing");
+    attroff(A_ITALIC | COLOR_PAIR(5));
+
+    int key = getch();
+    while(key != 'r' && key != 'R') {
+        key = getch();
+    }
+
+    erase();
 }
 
 bool game(void) {
@@ -80,18 +112,7 @@ bool game(void) {
         update_borders(borders, score);
 
         if(x_pos <= borders[LINES - 1] || x_pos >= borders[LINES - 1] + width) {
-            attron(A_BOLD | COLOR_PAIR(4));
-            mvaddch(LINES - 1, x_pos, 'X');
-            attroff(A_BOLD | COLOR_PAIR(4));
-
-            attron(A_BOLD | COLOR_PAIR(2));
-            mvprintw(LINES/2, COLS/2, "Game Over!");
-            mvprintw(LINES/2 + 1, COLS/2, "Score : %lu", score);
-            attroff(A_BOLD | COLOR_PAIR(2));
-
-            attron(A_ITALIC | COLOR_PAIR(2));
-            mvprintw(LINES/2 + 2, COLS/2 - 16, "Press 'p' to close the game or 'r' to restart");
-            attroff(A_ITALIC | COLOR_PAIR(2));
+            death(x_pos, score);
             break;
         }
 
@@ -188,5 +209,25 @@ void check_cheat(void) {
         printf("To play, please zoom in using ctrl + <3\n");
         printf("Currently trying to display %d/190 columns.\n", COLS);
         exit(0);
+    } else if(COLS < 110) {
+        endwin();
+        printf("To play, please zoom out using ctrl - <3\n");
+        printf("Currently trying to display %d/110 minimum columns (max is 190).\n", COLS);
+        exit(0);
     }
+}
+
+void death(unsigned x_pos, unsigned long score) {
+    attron(A_BOLD | COLOR_PAIR(4));
+    mvaddch(LINES - 1, x_pos, 'X');
+    attroff(A_BOLD | COLOR_PAIR(4));
+
+    attron(A_BOLD | COLOR_PAIR(2));
+    mvprintw(LINES/2, COLS/2, "Game Over!");
+    mvprintw(LINES/2 + 1, COLS/2, "Score : %lu", score);
+    attroff(A_BOLD | COLOR_PAIR(2));
+
+    attron(A_ITALIC | COLOR_PAIR(2));
+    mvprintw(LINES/2 + 2, COLS/2 - 16, "Press 'p' to close the game or 'r' to restart");
+    attroff(A_ITALIC | COLOR_PAIR(2));
 }
